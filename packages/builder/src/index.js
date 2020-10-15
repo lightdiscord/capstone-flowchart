1ec939022ef321db43c736dc94afa10dea31fbad
LightDiscord <root@arnaud.sh>
2020-10-16 01:28:37 +0200

    fix: undefined hash with contenthash for worker

--- packages/builder/src/index.js
+++ packages/builder/src/index.js
@@ -11,7 +11,7 @@ module.exports = {
     },
 
     output: {
-        filename: "[name].[contenthash].js",
+        filename: "[name].[chunkhash].js",
         path: path.resolve(__dirname, "..", "dist")
     },
 
--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import { store } from "./store/index.js";
 
-import "bulma/bulma.sass"
 import "./config/bulma.js";
 import "./config/font-awesome.js";
 
